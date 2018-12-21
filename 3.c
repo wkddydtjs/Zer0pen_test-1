6f9dafa75c0cb7c52579b8c287eb618ab610f413
-m <-m>
2018-12-21 10:37:25 +0900

    fix

--- 3.c
+++ 3.c
@@ -4,32 +4,32 @@ int main()
 	int arr[5][5] = { 0 };
 	int i, j;
 	int Isum = 0;
-	int Jsum = 0;
-	for (i = 0; i <4; i++)
+	int inp;
+	int max = 0;
+	int MaxI = 0;
+	for (i = 0; i < 5; i++)
 	{
 		for (j = 0; j < 4; j++)
 		{
-			arr[i][j] = i * 4 + j + 1;
+			scanf("%d", &inp);
+			arr[i][j] = inp;
+		}
+	}
+	for (i = 0; i < 5; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
 			Isum += arr[i][j];
 		}
 		arr[i][4] = Isum;
 		Isum = 0;
 	}
-	for (i = 0; i < 5; i++)
+	for (i = 0; i < 4; i++)
 	{
-		for (j = 0; j < 4; j++)
-		{
-			Jsum += arr[j][i];
-		}
-		arr[4][i] = Jsum;
-		Jsum = 0;
+		max = (arr[i][4]>arr[i + 1][4]) ? arr[i][4] : arr[i + 1][4];
+		MaxI = (arr[i][4]>arr[i + 1][4]) ? i + 1 : i + 2;
 	}
-	for (i = 0; i < 5; i++)
-	{
-		for (j = 0; j < 5; j++)
-		{
-			printf("%4d", arr[i][j]);
-		}
-		puts("");
-	}
+	printf("%d %d",MaxI, max);
+	puts("");
+
 }
